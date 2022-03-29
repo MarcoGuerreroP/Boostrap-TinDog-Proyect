f0aca338305cfd29c99acf61d8e7c7a8990d6d00
Marco Guerrero <MarcoGuerreroP>
2022-03-28 22:58:22 -0600

    Boostrap Tinder

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const horizontal = document.querySelector('.horizontal');
+var tag = '1.0.0';
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+
+
+
+}
+
+console.log(`%c${tag}`, 'color: #fff; background: #000; padding: 5px;');
+
